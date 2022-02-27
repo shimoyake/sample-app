329124b832fec4aaf771fb063a0a308134212b3d
shimoyake <yupu1205@gmail.com>
2022-02-27 15:37:06 +0900

    ルーティング修正:/でhomeのページへとばすように変更

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
 Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
